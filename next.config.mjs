adc4fc8dfd32bd455f7b4182a8984e22dc98b9b8
Asim Sk <blackhatcode786@gmail.com>
2024-10-29 23:07:09 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['img.icons8.com'], // Add other domains here if needed
+    },
+};
+
+export default nextConfig;
---
+++ src/lib/embedd.ts
@@ -0,0 +1,11 @@
+import { VoyageEmbeddings } from "@langchain/community/embeddings/voyage";
+
+const embeddings = new VoyageEmbeddings({
+  apiKey: process.env.CONVEX_API_KEY, // In Node.js defaults to process.env.VOYAGEAI_API_KEY
+  inputType: "document", // Optional: specify input type as 'query', 'document', or omit for None / Undefined / Null
+});
+
+export const embedd = async (text: string) => {
+  const embedding = await embeddings.embedQuery(text);
+  return embedding;
+};
