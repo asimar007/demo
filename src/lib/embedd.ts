e91dcadc37041c8658704496d23fa740acd3be8a
Asim Sk <blackhatcode786@gmail.com>
2024-10-30 09:58:13 +0530

    api4

--- src/lib/embedd.ts
+++ src/lib/embedd.ts
@@ -1,9 +1,9 @@
 import { VoyageEmbeddings } from "@langchain/community/embeddings/voyage";
 
-//const key = process.env.NEXT_PUBLIC_VOYAGEAI_API_KEY;
+const key = process.env.NEXT_PUBLIC_VOYAGEAI_API_KEY;
 
 const embeddings = new VoyageEmbeddings({
-  apiKey: "pa-Vr3W90Cn31QNFnpytJwyIl7Pk48HKQYrIb9hPvK8BtY", // In Node.js defaults to process.env.VOYAGEAI_API_KEY
+  apiKey: key, // In Node.js defaults to process.env.VOYAGEAI_API_KEY
   inputType: "document", // Optional: specify input type as 'query', 'document', or omit for None / Undefined / Null
 });
 
